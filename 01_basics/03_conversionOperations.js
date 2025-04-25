053a77ddce8ed0b03aa7776d56913993f50ed187
anshu0215 <goldenheavenlysunflower@gmail.com>
2025-04-25 18:30:49 +0000

    yo

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "anshu"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 
+
+           /* OPERATIONS */
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
