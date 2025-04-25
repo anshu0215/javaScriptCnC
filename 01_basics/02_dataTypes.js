69800cfa922fa6dbd1a56daa30d6d7cd26114211
anshu0215 <goldenheavenlysunflower@gmail.com>
2025-04-25 18:32:52 +0000

    data

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict": // means all JS code as newer version
+
+alert(3 + 3) // error because we are using nodejs, not browser
+
+let name = "anshu"
+let age = 18
+let isLoggedIn = false
+
+// number 
+// bigint
+// string ""
+// boolean true/false
+// null is a standalone value
+// undefined
+// symbol finds unique of a component
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+  // ***dataTypes*** 
+
+  // Premitive => stack
+  // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+  // Reference (Non primitive) => Heap
+  // Array, Object, Functions
